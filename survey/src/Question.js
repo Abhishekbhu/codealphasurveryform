7669136ac1b6530f670eb60d89d69a208adea484
Abhishekbhu <abhishek.bhumca22@gmail.com>
2023-08-24 14:23:10 +0530

    final

--- survey/src/Question.js
+++ survey/src/Question.js
@@ -1,147 +1,96 @@
-// const questionsAndAnswers = [
-//     {
-//       question: "Have you used our service before?",
-//       options: ["Paris", "London", "Berlin", "Rome"],
-//       answer: "Paris"
-//     },
-//     {
-//       question: "Which programming language is commonly used for web development?",
-//       options: ["Java", "Python", "JavaScript", "C++"],
-//       answer: "JavaScript"
-//     },
-//     {
-//       question: "What is the largest planet in our solar system?",
-//       options: ["Venus", "Jupiter", "Mars", "Saturn"],
-//       answer: "Jupiter"
-//     },
-//     {
-//       question: "Who is the author of the Harry Potter book series?",
-//       options: ["J.R.R. Tolkien", "J.K. Rowling", "George R.R. Martin", "Stephen King"],
-//       answer: "J.K. Rowling"
-//     },
-//     {
-//       question: "Which country is famous for the Taj Mahal?",
-//       options: ["India", "China", "Egypt", "Brazil"],
-//       answer: "India"
-//     },
-//     {
-//       question: "What does HTML stand for?",
-//       options: ["Hyper Text Markup Language", "Home Tool Markup Language", "Hyperlinks and Text Markup Language", "Hypertext Transfer Protocol"],
-//       answer: "Hyper Text Markup Language"
-//     },
-//     {
-//       question: "What is the chemical symbol for water?",
-//       options: ["O", "H2O", "W", "H2"],
-//       answer: "H2O"
-//     },
-//     {
-//       question: "Which planet is known as the 'Red Planet'?",
-//       options: ["Venus", "Jupiter", "Mars", "Saturn"],
-//       answer: "Mars"
-//     },
-//     {
-//       question: "What is the main function of the CPU in a computer?",
-//       options: ["Display images on the screen", "Store data", "Perform calculations", "Connect to the internet"],
-//       answer: "Perform calculations"
-//     },
-//     {
-//       question: "What is the largest mammal on Earth?",
-//       options: ["Elephant",  "Blue Whale", "Giraffe", "Lion"],
-//       answer: "Blue Whale"
-//     }
-//   ];
+
   
 const questionsAndAnswers = [
   {
-    question: "Have you used our service before?",
+    question: "How do you usually spend your weekends?",
     options: [
-      "Yes, regularly",
-      "Yes, occasionally",
-      "No, familiar with it",
-      "No, never heard of it"
-    ]
+      "Watching movies",
+      "Going for a hike",
+      "Reading books",
+      "Hanging out with friends",
+    ],
   },
   {
-    question: "On a scale of 1 to 10, how satisfied are you with our service?",
+    question: "What's your favorite type of cuisine?",
     options: [
-      "1 (Very Dissatisfied)",
-      "4 (Neutral)",
-      "7",
-      "10 (Very Satisfied)"
-    ]
+      "Italian",
+      "Mexican",
+      "Japanese",
+      "Indian",
+    ],
   },
   {
-    question: "What aspects of our service do you find most valuable? (Select one)",
+    question: "What's your dream vacation destination?",
     options: [
-      "Convenience",
-      "Quality",
-      "Customer Service",
-      "Affordability"
-    ]
+      "Paris, France",
+      "Bora Bora, French Polynesia",
+      "Tokyo, Japan",
+      "Machu Picchu, Peru",
+    ],
   },
   {
-    question: "What improvements or changes would enhance your experience with our service?",
+    question: "If you could have any superpower, what would it be?",
     options: [
-      "Faster service",
-      "More variety",
-      "Better communication",
-      "Other (please specify)"
-    ]
+      "Invisibility",
+      "Flight",
+      "Teleportation",
+      "Super strength",
+    ],
   },
   {
-    question: "How often do you use our service?",
+    question: "What's your favorite season of the year?",
     options: [
-      "Daily",
-      "Weekly",
-      "Monthly",
-      "Rarely"
-    ]
+      "Spring",
+      "Summer",
+      "Fall",
+      "Winter",
+    ],
   },
   {
-    question: "How likely are you to recommend our service to friends or family?",
+    question: "Do you prefer in drink?",
     options: [
-      "Extremely Likely",
-      "Likely",
-      "Unlikely",
-      "Extremely Unlikely"
-    ]
+      "Coffee",
+      "Tea",
+      "Wine",
+      "Soup"
+    ],
   },
   {
-    question: "What influenced your decision to choose our service over alternatives?",
+    question: "What's your go-to dessert?",
     options: [
-      "Reputation",
-      "Pricing",
-      "Features",
-      "Other (please specify)"
-    ]
+      "Chocolate cake",
+      "Ice cream",
+      "Fruit salad",
+      "Cheesecake",
+    ],
   },
   {
-    question: "Have you encountered any issues while using our service?",
+    question: "What's your favorite outdoor activity?",
     options: [
-      "Yes",
-      "No",
+      "Cycling",
+      "Swimming",
+      "Playing sports",
+      "Picnicking",
+    ],
+  },
+  {
+    question: "Do you enjoy cooking at home?",
+    options: [
+      "Yes, I love it",
       "Sometimes",
-      "very often"
-    ]
+      "No, I prefer dining out",
+      "Never Ever"
+    ],
   },
   {
-    question: "If you answered 'Yes' to the previous question, please describe the issues you've encountered.",
+    question: "What's your preferred mode of transportation?",
     options: [
-      "Technical glitches",
-      "Poor customer service",
-      "Billing problems",
-      "Other (please specify)"
-    ]
+      "Car",
+      "Public transit",
+      "Bicycle",
+      "Walking",
+    ],
   },
-  {
-    question: "Would you be interested in participating in future user testing or providing additional feedback?",
-    options: [
-      "Yes",
-      "No",
-      "maybe",
-      "never ever"
-    ]
-  }
 ];
 
   export default questionsAndAnswers;
